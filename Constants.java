566d01906b13625693cef9293468f185a65e1d1a
Caleb <calebkugel1@gmail.com>
2023-02-12 17:21:11 -0600

    updated constants

--- Constants.java
+++ Constants.java
@@ -2,9 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package frc.robot.Newman_Constants;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 
 /**
@@ -39,7 +40,7 @@ public final class Constants {
 
     public final static int CAN_hopperright = 16;
     public final static int CAN_hopperleft = 17;
-    
+
     public final static int CAN_PNM = 18;
 
     /**
