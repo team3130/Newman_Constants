74011a9948ff204082a79b9189b35a03fcf09089
Caleb <calebkugel1@gmail.com>
2023-02-15 16:39:37 -0600

    switched to be punchy limit switch

--- Constants.java
+++ Constants.java
@@ -56,8 +56,7 @@ public final class Constants {
     /**
      * Digital inputs
      */
-    public static final int RIGHT_LIMIT_SWITCHES = 2;
-    public static final int LEFT_LIMIT_SWITCHES = 3;
+    public static final int PUNCHY_LIMIT_SWITCH = 2;
 
     // Order should match side
     public static final int[] turningId = new int[] {CAN_LeftFrontSteer, CAN_LeftBackSteer, CAN_RightFrontSteer, CAN_RightBackSteer};
