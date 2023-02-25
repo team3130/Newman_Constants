aeeb3aaf585a18506a27b09b441921186dcd382e
Caleb <calebkugel1@gmail.com>
2023-02-24 23:14:43 -0600

    optimized imports

--- Constants.java
+++ Constants.java
@@ -4,12 +4,9 @@
 
 package frc.robot.Newman_Constants;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.math.Pair;
-import edu.wpi.first.math.geometry.Translation2d;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
