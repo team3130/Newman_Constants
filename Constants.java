35096d0e1a01083ca37891bec854bdc39a134dd3
Caleb <calebkugel1@gmail.com>
2023-04-10 10:25:21 -0500

    pitch offset for navx

--- Constants.java
+++ Constants.java
@@ -97,6 +97,8 @@ public final class Constants {
     public final static double kMaxExtensionArmVoltage = 10.5d;
     public final static double kMaxVoltageHopper = 9d;
 
+    public final static double PitchOffset = -12; // degrees
+
     /** Chassis auton */
     public static final double kPXController = 3;
     public static final double kIXController = 0.5;
